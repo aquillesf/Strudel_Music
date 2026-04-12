a6b9df0be189876a5ed5d1ca4fc0a33421526268
Aquiles <57549900+aquillesf@users.noreply.github.com>
2026-04-12 05:35:39 -0300

    Refactor sound settings and adjust gain values

--- newlowkamb#3.js
+++ newlowkamb#3.js
@@ -10,26 +10,36 @@ let intro =
   .legato(1.2)
   .slow(3)
   .struct("x [x x] x x*2")
-  .room(0.9)
-  .size(0.1)
-  .delay("0.3")
-  .delayfeedback(0.85)
+  .room(0.9).size(0.1)
+  .delay("0.3").delayfeedback(0.85)
   .gain(0.28)
   .lpf(perlin.range(300, 4000))
   .hpf(sine.range(0, 800))
 
-let soundintro = s("r88_bd").struct("~ ~ [x x] ~ ~").gain(0.03).room(.3).pan(0.7)
+let soundintro = s("r88_bd").struct("~ ~ [x x] ~ ~").gain(0.01).room(.3).pan(0.7)
 
-let cbintro = s("spacedrum_cb").struct("~ ~ x ~").gain(0.2).room(.3).lpf(1300).pan(0.3)
+let cbintro = s("spacedrum_cb").struct("~ ~ x ~").gain(0.02).room(.3).lpf(1300).pan(0.3)
 
-let introsine = s("waspinsectbling").note("<F2 F3>").gain(0.1).room(.3).lpf(1300).pan(0.5).delay("0.6")
+let introsine = s("waspinsectbling").note("<F2>").gain(0.02).room(.3).lpf(1300).pan(0.5).delay("0.6")
 
-let isqintro = s("rolandtr626_sh").struct("~ ~ ~ x").gain(0.02).room(0.7).pan(1.3)
+let isqintro = s("rolandtr626_sh").struct("~ ~ ~ x").gain(0.008).room(0.7).pan(1.3).delay("0.1")
+
+let melodia = intro
+
+let perc = arrange(
+  [1, stack(soundintro)],
+  [3, silence],
+  [1, stack(isqintro)],
+  [2, silence],
+  [1, stack(cbintro, soundintro)],
+  [2, silence],
+  [1, stack(introsine, isqintro)],
+  [4, silence],
+  [1, stack(soundintro, cbintro, introsine, isqintro)],
+)
 
 let song = arrange(
-    [2, stack(intro, soundintro)],
-    [3, stack(intro)],
-    [1, stack(intro, isqintro)],
+  [15, stack(perc, intro)],
 )
 
 song
